f7923f5581c5bc74fba0c03e799e65264e37f041
Rithwik0I <2200033309@kluniversity.in>
2024-05-25 16:58:57 +0800

    Removed a lot react

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -6,7 +6,6 @@ import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
 
 export default function Hero() {
   return (
