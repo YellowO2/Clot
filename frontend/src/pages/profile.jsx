7911be21c7ab42b0a83fd217f73d9e73be7ca5e0
bliblublob <huangyuxuan121122@gmail.com>
2024-05-25 17:53:36 +0800

    Style: Search bar

--- frontend/src/pages/profile.jsx
+++ frontend/src/pages/profile.jsx
@@ -1,17 +1,52 @@
 import React from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Avatar from "@material-ui/core/Avatar";
+// import Typography from "@material-ui/core/Typography";
+// import Paper from "@material-ui/core/Paper";
 
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
+//     padding: theme.spacing(3),
+//   },
+//   avatar: {
+//     width: theme.spacing(10),
+//     height: theme.spacing(10),
+//     marginBottom: theme.spacing(2),
+//   },
+// }));
 
+const ProfilePage = () => {
+  //   const classes = useStyles();
 
-const Profile = () => {
+  return (
+    <></>
+    // <Paper elevation={3}>
+    //   <Avatar
+    //     alt="User Avatar"
+    //     src="/path/to/avatar.jpg"
+    //     // className={classes.avatar}
+    //   />
+    //   <Typography variant="h5" gutterBottom>
+    //     John Doe
+    //   </Typography>
+    //   <Typography variant="body1" gutterBottom>
+    //     Age: 30
+    //   </Typography>
+    //   <Typography variant="body1" gutterBottom>
+    //     Email: johndoe@example.com
+    //   </Typography>
+    //   <Typography variant="body1" gutterBottom>
+    //     Location: New York
+    //   </Typography>
+    //   <Typography variant="body1">
+    //     Bio: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vel
+    //     aliquet velit, vitae varius nulla.
+    //   </Typography>
+    // </Paper>
+  );
+};
 
-
-
-
-
-
-
-}
-   
-export default Profile;
-
-
+export default ProfilePage;
